4dffd033df2577018ad42a7ddeae976f91c6e947
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-26 19:59:35 +0600

    statistics page pie chart full functionality finished propely

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { PieChart, Pie,  Cell, Label } from 'recharts';
+import { PieChart, Pie,  Cell } from 'recharts';
 
 
 const Statistics = () => {
@@ -30,27 +30,47 @@ const Statistics = () => {
 
    const COLORS = ['#FF444A', '#00C49F'];
 
+   const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
     return (
         
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center">
           <PieChart width={400} height={400}>
           <Pie
             data={data}
             cx="50%"
             cy="50%"
-            labelLine={false}
+            labelLine={true}
+            label={renderCustomizedLabel}
             outerRadius={80}
             fill="#8884d8"
             dataKey="value"
           >
             {data.map((entry, index) => (
-              <Cell key={`label-${index}`} fill={COLORS[index % COLORS.length]} />
+              <Cell key={`cell-${index}`}
+               fill={COLORS[index % COLORS.length]} />
             ))}
           </Pie>
         </PieChart>
-          
-         
-        </div>
+        <div>
+           <div className="text-[#0B0B0B] text-sm text-center font-normal"><h3>Your Donation</h3></div>
+            <div className="w-25 h-3 bg-[#00C49F] rounded-sm"></div>
+            <div className="text-[#0B0B0B] text-sm text-center font-normal"><h3>Total Donation</h3></div>
+            <div className="w-25 h-3 bg-[#FF444A] rounded-sm"></div>
+            </div>
+
+          </div>
 
         
     );
